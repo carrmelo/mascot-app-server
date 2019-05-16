52c3701e53c7cbe0f3dc662d921f97e175fa17a3
car <carmelogustavo@gmail.com>
2019-05-16 12:19:20 +0200

    refactor: change port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const conf = require('./config');
 const router = require('./router');
 
 const app = new Koa();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app
   .use(cors())
